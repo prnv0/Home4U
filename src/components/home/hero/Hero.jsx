6e01b2f2e5587a1ff34f481722d8b20496d6d862
Pranav Satish <117537359+prnv0@users.noreply.github.com>
2023-11-13 01:54:59 +0530

    finalized homepage

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -1,13 +1,22 @@
-import React from "react"
+import React, { useState } from "react";
 import Heading from "../../common/Heading"
+import DropdownList from "react-widgets/DropdownList";
+import "react-widgets/styles.css";
 import "./hero.css"
 
 const Hero = () => {
+  const [showMoreMenu, setShowMoreMenu] = useState(false);
+
+  const toggleMoreMenu = (e) => {
+    e.preventDefault(); // Prevent the default form submission behavior
+    setShowMoreMenu(!showMoreMenu);
+  };
+  
   return (
     <>
       <section className='hero'>
         <div className='container'>
-          <Heading title='Search Your Next Home ' subtitle='Find new & featured property located in your local city.' />
+          <Heading title='Your new Home Awaits! ' subtitle='Find new & featured property located in your local city.' />
 
           <form className='flex'>
             <div className='box'>
@@ -15,20 +24,35 @@ const Hero = () => {
               <input type='text' placeholder='Location' />
             </div>
             <div className='box'>
+              <span>Listing Type</span>
+              <DropdownList defaultValue="Buy" data={["Buy", "Rent", "Lease"]}/>
+            </div>
+            <div className='box'>
               <span>Property Type</span>
-              <input type='text' placeholder='Property Type' />
+              <DropdownList defaultValue="House" data={["House", "Villa", "Apartment", "Office"]}/>
             </div>
-            <div className='box'>
-              <span>Price Range</span>
-              <input type='text' placeholder='Price Range' />
+            <div className="additional">
+              <button className='btnmore' onClick={(e) => toggleMoreMenu(e)}>
+                <h4>More</h4>
+              </button>
+              <button className='btn1'>
+                <i className='fa fa-search'></i>
+              </button>
             </div>
-            <div className='box'>
-              <h4>Advance Filter</h4>
-            </div>
-            <button className='btn1'>
-              <i className='fa fa-search'></i>
-            </button>
+            {showMoreMenu && (
+              <>
+                <div className='box'>
+                  <span>Price Range</span>
+                  <input type='text' placeholder='Field 1' />
+                </div>
+                <div className='box'>
+                  <span>Bedrooms</span>
+                  <DropdownList defaultValue="2BHK" data={["1BHK", "2BHK", "3BHK", "4BHK"]}/>
+                </div>
+              </>
+            )}
           </form>
+          
         </div>
       </section>
     </>
